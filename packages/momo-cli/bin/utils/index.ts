4aa8abf3716e7bf8d7670ed4d0dd06238031c1a5
Moonlightwsn <543456277@qq.com>
2023-01-24 15:38:45 +0800

    chore: update .gitignore

--- packages/momo-cli/bin/utils/index.ts
+++ packages/momo-cli/bin/utils/index.ts
@@ -1,8 +1,7 @@
 import path from "path"
 import { fileURLToPath } from "url"
-
 const __dirname = fileURLToPath(import.meta.url)
 
-export const getCliRootPath = (pathUrl: string) => {
-  return path.resolve(__dirname, `../../../${pathUrl}`)
+export const getCliRootPath = (pathUrl?: string) => {
+  return path.resolve(__dirname, `../../../${pathUrl || ""}`)
 }
